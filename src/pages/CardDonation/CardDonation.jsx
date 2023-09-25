09d5c8180d787399afea916581a0072c83fe9584
mahizaman490 <mahizaman490@gmail.com>
2023-09-26 01:22:18 +0600

    created donation component and set all the data in local storage

--- src/pages/CardDonation/CardDonation.jsx
+++ src/pages/CardDonation/CardDonation.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
+import SingleDonationCard from "./SingleDonationCard";
 
 const CardDonation = () => {
     const [card, setCard] = useState({})
@@ -18,18 +19,9 @@ const CardDonation = () => {
 
     console.log(card)
     return (
-        <div>
-         <div className="relative">
-            <img className="rounded-lg " src="https://i.ibb.co/s2mJzzn/Rectangle-4282.png" alt="" />
-         <div style={{ position: 'absolute', width:'100%',height: '20%',left: '0', right: '0',bottom:'0', backgroundColor: 'rgba(0,0,0,0.5)', zIndex:'2'}}> 
-         <button className="btn">Donate</button>
-          </div>
-        </div>
-        <div className="py-5 px-2">
-            <h1 className="text-xl font-bold">Good Education</h1>
-            <small className="text-slate-600">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quis facere illo animi vel amet eos alias deserunt, similique at earum nesciunt ex laudantium sapiente quia expedita adipisci, repudiandae fuga necessitatibus vitae dolorem. Asperiores obcaecati, fugit rerum vitae perferendis, illo, sunt maxime doloremque mollitia ut quidem sapiente! Molestiae sequi ullam incidunt?</small>
-        </div>
-        </div >
+       <div>
+        <SingleDonationCard card={card}></SingleDonationCard>
+       </div>
     );
 };
 
---
+++ src/pages/CardDonation/SingleDonationCard.jsx
@@ -0,0 +1,49 @@
+
+const SingleDonationCard = ({card}) => {
+    const {id, picture, title, text_color, description, price } = card || {}
+
+    const buttonStyle = {
+    
+        backgroundColor: text_color,
+    
+    };
+
+    const handleAddToDonation = () =>{
+
+        const addedDonationArray = []
+        const DonationItems = JSON.parse(localStorage.getItem('donation'))
+        if(!DonationItems){
+            addedDonationArray.push(card)
+            localStorage.setItem('donation',JSON.stringify(addedDonationArray))
+            alert('Your Donation successful')
+        }else{
+            const isExits = DonationItems.find(card => card.id === id)
+
+            if(!isExits){
+                addedDonationArray.push(...DonationItems,card)
+            localStorage.setItem('donation',JSON.stringify(addedDonationArray))
+            alert('Your Donation successful')  
+
+            }else{
+               alert('already donated') 
+            }
+         
+        }
+    }
+    return (
+        <div className=" ">
+        <div className="relative h-[450px]">
+           <img className=" w-full md:h-full " src={picture} alt="" />
+        <div style={{ position: 'absolute', width:'100%',height: '20%',left: '0', right: '0',bottom:'0', backgroundColor: 'rgba(0,0,0,0.5)', zIndex:'2'}}> 
+        <button onClick={handleAddToDonation} className="btn py-3 ml-3 text-white px-3 mt-3 rounded-md md:mt-6 lg:mt-6" style={buttonStyle} >Donate ${price}</button>
+         </div>
+       </div>
+       <div className="py-5 px-2">
+           <h1 className="text-xl font-bold">{title}</h1>
+           <small className="text-slate-600">{description}</small>
+       </div>
+       </div >
+    );
+};
+
+export default SingleDonationCard;
