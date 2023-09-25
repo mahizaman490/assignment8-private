4fcd6c64530ee6fba7d0b281fbc9ec1d1bb9447f
mahizaman490 <mahizaman490@gmail.com>
2023-09-25 16:37:09 +0600

    created style object for color

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -1,14 +1,27 @@
 const Card = ({ card }) => {
     const { id, picture, title, category_bg, card_bg, text_color, description, price, category } = card || {}
+
+    const cardStyle = {
+        backgroundColor: card_bg,
+    };
+    const buttonStyle = {
+        color: text_color,
+        backgroundColor: category_bg,
+    
+    };
+    const titleStyle = {
+        color: text_color,
+    };
+
     return (
         <div>
-            <div className="card md:w-56 card-compact bg-base-100 shadow-xl">
+            <div className="card md:w-56 card-compact bg-base-100 shadow-xl" style={cardStyle}>
                 <figure ><img src={picture} alt="" /></figure>
                 <div className="card-body p-4">
                     <div className="card-actions">
-                        <button className="btn btn-primary">{category}</button>
+                        <button className="btn py-1 px-3 rounded-md" style={buttonStyle}>{category}</button>
                     </div>
-                    <h2 className="card-title">{title}</h2>
+                    <h2 className="card-title" style={titleStyle}>{title}</h2>
 
 
                 </div>
