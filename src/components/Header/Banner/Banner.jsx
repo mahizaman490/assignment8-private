80ff80c62a0ba53c6346c8bd5aae81e3cbc2fbf2
mahizaman490 <mahizaman490@gmail.com>
2023-09-27 03:38:44 +0600

    created the filter function for input field

--- src/components/Header/Banner/Banner.jsx
+++ src/components/Header/Banner/Banner.jsx
@@ -1,6 +1,13 @@
+import { useState } from "react";
 
 
-const Banner = () => {
+const Banner = ({setSearchTerm}) => {
+    const [text, setSearchText] = useState('');
+      
+    const handleSubmit = (text) =>{
+        setSearchTerm(text)
+
+    }
     return (
         <div>
 
@@ -9,13 +16,13 @@ const Banner = () => {
                 <div className="relative">
                     <img className=" opacity-10 ..." src="https://i.ibb.co/s2mJzzn/Rectangle-4282.png" alt="" />
                     <div>
-                        <div className="absolute left-10 md:left-16 right-6 top-5 md:top-32 lg:top-36 lg:left-72 lg:right-2 lg:bottom-1/4 z-20 lg:h-2/6 lg:w-6/12 text-center items-center justify-center" >
+                        <div className="absolute left-10 md:left-16 right-6 top-5 md:top-32 lg:top-44 lg:left-72 lg:right-2 lg:bottom-1/4 z-20 lg:h-2/6 lg:w-6/12 text-center items-center justify-center" >
                             <h1 className="md:text-3xl font-bold">
                              
                                 I Grow By Helping People In Need</h1>
                             <div>
-                                <input type="text" placeholder="Search here..." className="input input-bordered w-full max-w-xs px-3 py-1 md:py-3" />
-                                <button className="btn py-1 md:py-3  text-white bg-red-600 px-3 mt-1 md:mt-3 rounded-md md:mt-6 lg:mt-6">Search</button>
+                                <input onChange={(e)=>setSearchText(e.target.value)} type="text" text={text} placeholder="Search here..." className="input input-bordered w-full max-w-xs px-3 py-1 md:py-3" />
+                                <button onClick={()=>handleSubmit(text)} className="btn py-1 md:py-3  text-white bg-red-600 px-3 mt-1 md:mt-3 rounded-md md:mt-6 lg:mt-6">Search</button>
                             </div>
 
                         </div>
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,16 +1,36 @@
 
 import { useLoaderData } from 'react-router-dom';
-import Cards from '../../components/Cards/Cards';
+// import Cards from '../../components/Cards/Cards';
 import Banner from '../../components/Header/Banner/Banner';
+import { useEffect, useState } from 'react';
+import Card from '../../components/Cards/Card';
 
 const Home = () => {
     const cards = useLoaderData()
-  
+   const [searchTerm, setSearchTerm] = useState('');
+    const [filteredData, setFilteredData] = useState([]);
+
+   
+
+    useEffect(()=>{
+     fillterData()
+    },[searchTerm])
+
+    const fillterData = ()=>{
+        const filteredData = cards.filter(item => item.category.toLowerCase().includes(searchTerm.toLowerCase()))
+        setFilteredData(filteredData)
+
+    }
     return (
         <>
          <div>
-          <Banner></Banner> 
-          <Cards cards={cards}></Cards> 
+          <Banner setSearchTerm={setSearchTerm}></Banner> 
+          <div className='grid mt-8 grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-5 justify-items-center'>
+            {
+                filteredData.map(card => <Card key={card.id} card={card}> </Card> )
+            }
+          </div>
+          {/* <Cards cards={cards}></Cards>  */}
          </div>  
       
         </>
